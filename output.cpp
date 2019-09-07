b8597e1a011376328d4d3475f1603e2141bc037e
yashmini <yashmini.bittu13may@gmail.com>
2019-09-07 17:43:18 +0530

    raptor basic class

---
+++ output.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? x;
+
+   raptor_prompt_variable_zzyz ="my name is:";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> x;
+   cout << "yashmini" << endl;
+   return 0;
+}
---
+++ string concate.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? x;
+   ?? y;
+   ?? c;
+
+   raptor_prompt_variable_zzyz ="str1";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> x;
+   raptor_prompt_variable_zzyz ="str2";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> y;
+   c =x+" "+y;
+   cout << "string:"+c << endl;
+   return 0;
+}
